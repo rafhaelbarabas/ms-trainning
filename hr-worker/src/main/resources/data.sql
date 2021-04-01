af03270810a6991ebd9536fdae9c1ca42489800f
Rafhael de Andrade Barabas <rafhael_ab@hotmail.com>
2021-04-01 17:02:00 -0300

    config eureka clients

--- hr-worker/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO worker (name, daily_income) VALUES ('Bob', 200.00)
-INSERT INTO worker (name, daily_income) VALUES ('Tony', 300.00)
-INSERT INTO worker (name, daily_income) VALUES ('Jack', 150.00)
